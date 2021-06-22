9ee437780d7219765c9281667d364655de336c00
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-06-21 20:06:30 -0400

    Application completed

--- QA-Midterm/Program.cs
+++ QA-Midterm/Program.cs
@@ -8,8 +8,127 @@ namespace QA_Midterm
 {
     internal class Program
     {
+        public static string Input;
+        public static string output;
+        private static int integerCommand;
+        private static int integerDimension;
+        private static int dimensionA;
+        private static int dimensionB;
+        private static int dimensionC;
+        private static int inputDimesions = 0;
+
         private static void Main(string[] args)
         {
+            while (true)
+            {
+                InitiateMenuSelection();
+                ExecuteMenuCommand();
+                ResetAllNumbers();
+            }
+        }
+
+        private static void InitiateMenuSelection()
+        {
+            ShowMenu();
+            ReadUserInput();
+            ValidateMenuSelection();
+        }
+
+        private static void ExecuteMenuCommand()
+        {
+            switch (integerCommand)
+            {
+                case 1:
+                    GetDimensions();
+                    output = TriangleSolver.Analyze(dimensionA, dimensionB, dimensionC);
+                    Console.WriteLine(output);
+                    Console.WriteLine("*** *** ***");
+                    break;
+
+                case 2:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private static void ValidateMenuSelection()
+        {
+            IsInputInteger();
+            CheckInputRage();
+        }
+
+        private static void CheckInputRage()
+        {
+            if (integerCommand > 2 || integerCommand < 1)
+            {
+                InitiateMenuSelection();
+            }
+        }
+
+        private static void IsInputInteger()
+        {
+            if (!int.TryParse(Input, out integerCommand))
+            {
+                InitiateMenuSelection();
+            }
+        }
+
+        private static void GetDimensions()
+        {
+            Console.WriteLine("Enter a trianlge dimension");
+            ReadUserInput();
+            IsDimensionInteger();
+            inputDimesions++;
+
+            switch (inputDimesions)
+            {
+                case 1:
+                    dimensionA = integerDimension;
+                    GetDimensions();
+                    break;
+
+                case 2:
+                    dimensionB = integerDimension;
+                    GetDimensions();
+                    break;
+
+                case 3:
+                    dimensionC = integerDimension;
+                    break;
+            }
+
+            while (inputDimesions < 3)
+            {
+                GetDimensions();
+            }
+        }
+
+        private static void IsDimensionInteger()
+        {
+            if (!int.TryParse(Input, out integerDimension))
+            {
+                Console.WriteLine("Integer mate!!!!");
+                GetDimensions();
+            }
+        }
+
+        private static void ReadUserInput()
+        {
+            Input = Console.ReadLine();
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("1. Enter trianlge dimensions");
+            Console.WriteLine("2. Exit");
+        }
+
+        private static void ResetAllNumbers()
+        {
+            inputDimesions = 0;
+            dimensionA = 0;
+            dimensionB = 0;
+            dimensionC = 0;
         }
     }
 }
---
+++ QA-Midterm/TriangleSolver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Midterm
+{
+    public static class TriangleSolver
+    {
+        private static string triangleType;
+        private const string IS_NOT_A_TRIANGLE = "The numbers do not form a triangle.";
+        private const string IS_A_TRIANGLE = "The numbers form a triangle.\n";
+        private const string IS_EQUILATERAL = "The trianlge is equilateral.";
+        private const string IS_ISOSCELES = "The trianlge is isosceles.";
+        private const string IS_SCALENE = "The trianlge is scalene.";
+
+        public static string Analyze(int dimensionA, int dimensionB, int dimensionC)
+        {
+            if (!IsTriangle(dimensionA, dimensionB, dimensionC))
+            {
+                return IS_NOT_A_TRIANGLE;
+            }
+
+            triangleType = AnalyzeTriangleType(dimensionA, dimensionB, dimensionC);
+
+            return IS_A_TRIANGLE + triangleType;
+        }
+
+        private static bool IsTriangle(int dimensionA, int dimensionB, int dimensionC)
+        {
+            int AB = dimensionA + dimensionB;
+            int BC = dimensionB + dimensionC;
+            int CA = dimensionC + dimensionA;
+
+            if (AB < dimensionC || BC < dimensionA || CA < dimensionB)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static string AnalyzeTriangleType(int dimensionA, int dimensionB, int dimensionC)
+        {
+            if (dimensionA == dimensionB && dimensionB == dimensionC)
+            {
+                return IS_EQUILATERAL;
+            }
+
+            if (dimensionA == dimensionB || dimensionB == dimensionC)
+            {
+                return IS_ISOSCELES;
+            }
+
+            return IS_SCALENE;
+        }
+    }
+}
